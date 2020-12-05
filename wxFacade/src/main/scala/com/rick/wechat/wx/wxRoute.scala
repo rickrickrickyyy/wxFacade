494ee31e21e0e3fcd8c9ebe1ce9763b2d8adaaa0
rick <contou.y@gmail.com>
2020-12-05 15:13:16 +0800

    1.完善wxRoute.scala

--- wxFacade/src/main/scala/com/rick/wechat/wx/wxRoute.scala
+++ wxFacade/src/main/scala/com/rick/wechat/wx/wxRoute.scala
@@ -5,20 +5,31 @@ import faithful.Future
 
 import scala.scalajs.js
 import scala.scalajs.js.UndefOr
+//TODO:
+class EventChannel()
 
+private class RouteCallback[T <: js.Object](path: String, data: Option[T] = None) extends navigateToCallBack[T, Unit](path, data)
 
-private class navigateTo[T <: js.Object](path: String, data: Option[T] = None) extends Callback[T] with Url {
+private class navigateToCallBack[T <: js.Object, EventChannel](path: String, data: Option[T] = None) extends Callback[EventChannel] with Url {
   override val url: UndefOr[String] = js.defined {
     path + data.map(query).getOrElse("")
   }
 }
 
+object switchTab {
+  def apply(path: String): Future[Unit] = new RouteCallback(path, None).future(wxObject.switchTab)
+}
+
+object reLaunch {
+  def apply[T <: js.Object](path: String, data: Option[T] = None): Future[Unit] = new RouteCallback(path, data).future(wxObject.reLaunch)
+}
+
 object navigateTo {
-  def apply[T <: js.Object](path: String, data: Option[T] = None): Future[UndefOr[T]] = new navigateTo(path, data).future(wxObject.navigateTo)
+  def apply[T <: js.Object](path: String, data: Option[T] = None): Future[EventChannel] = new navigateToCallBack[T, EventChannel](path, data).future(wxObject.navigateTo)
 }
 
 object redirectTo {
-  def apply[T <: js.Object](path: String, data: Option[T] = None): Future[T] = new navigateTo(path, data).future(wxObject.redirectTo)
+  def apply[T <: js.Object](path: String, data: Option[T] = None): Future[Unit] = new RouteCallback(path, data).future(wxObject.redirectTo)
 }
 
 private class navigateBack(val delta: UndefOr[Int] = js.undefined) extends Callback[Unit]
